cdf6fea392e7836226450e1da0260c294bd9e48e
shyank akshar <akshar@shyankdev.us>
2024-06-03 20:47:04 +0530

    update network layer

--- MIPS_iOS_SDK/Networking/NetworkAdaptor.swift
+++ MIPS_iOS_SDK/Networking/NetworkAdaptor.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Alamofire
 
 
 public class NetworkAdaptor {
@@ -17,10 +16,8 @@ public class NetworkAdaptor {
         mipsNetworkURl : MipsNetworkUrls ,
         merchant : MerchantDetails ,
         credentials : MerchantCredentials ,
-        orderID : String ,
-        completion : @escaping ( (_ isPaymentCompleted : Bool) -> Void) ,
-        errorHandler : @escaping  ( (_ errorString : String) -> Void )
-    ){
+        orderID : String
+    ) async -> Bool {
         let json = [
             "authentify" : [
                 "id_merchant" : merchant.sIdMerchant ,
@@ -37,43 +34,70 @@ public class NetworkAdaptor {
         let (body , encodeError) = JSONCoder.encodeJson(json: json)
         
         if let error = encodeError {
-            errorHandler(error.localizedDescription)
-            return
+            LogManager.shared.error(error.localizedDescription)
+            return false
         }
         
-        AF.upload(
-            body ?? .init(),
-            to: mipsNetworkURl.paymentConfirmationUrl ,
-            method: .post ,
-            headers: .init([
-                .init(
-                    name: "Authorization",
-                    value: "Basic \(getBasicAuthWithusernamePassword(username: credentials.username, password: credentials.password))"
-                )
-            ])
-        )
-        .response { afData in
-            if let error = afData.error {
-                errorHandler(error.localizedDescription)
-                return
-            }
-            guard let data = afData.data
-            else{
-                errorHandler("no data found from server")
-                return
-            }
-            let (json , err) = JSONCoder.decodeJson(data: data)
-            if let err = err {
-                errorHandler(err.localizedDescription)
-                return
-            }
-            let status = json["order_status"] as? String  ?? ""
-            if status.lowercased() == "success" {
-                completion(true)
-                return
-            }
-            completion(false)
+        let req = NSMutableURLRequest(url: URL(string: mipsNetworkURl.paymentConfirmationUrl)!  )
+        
+        req.httpMethod = "POST"
+        req.setValue("Basic \(getBasicAuthWithusernamePassword(username: credentials.username, password: credentials.password))", forHTTPHeaderField: "Authorization")
+        req.httpBody = body
+        
+        let (res , resErr) = await HttpHandler.request(req as URLRequest)
+        if let err = resErr {
+            LogManager.shared.error(err.localizedDescription)
+            return false
         }
+        
+        let (decode , decodeErr) = JSONCoder.decodeJson(data: res!)
+        if let decodeErr = decodeErr {
+            LogManager.shared.error(decodeErr.localizedDescription)
+            return false
+        }
+        
+        let status = decode["order_status"] as? String  ?? ""
+        
+        if status.lowercased() == "success" {
+            return true
+        }else{
+            return false
+        }
+        
+        
+//        AF.upload(
+//            body ?? .init(),
+//            to: mipsNetworkURl.paymentConfirmationUrl ,
+//            method: .post ,
+//            headers: .init([
+//                .init(
+//                    name: "Authorization",
+//                    value: "Basic \(getBasicAuthWithusernamePassword(username: credentials.username, password: credentials.password))"
+//                )
+//            ])
+//        )
+//        .response { afData in
+//            if let error = afData.error {
+//                errorHandler(error.localizedDescription)
+//                return
+//            }
+//            guard let data = afData.data
+//            else{
+//                errorHandler("no data found from server")
+//                return
+//            }
+//            let (json , err) = JSONCoder.decodeJson(data: data)
+//            if let err = err {
+//                errorHandler(err.localizedDescription)
+//                return
+//            }
+//            let status = json["order_status"] as? String  ?? ""
+//            if status.lowercased() == "success" {
+//                completion(true)
+//                return
+//            }
+//            completion(false)
+//        }
     }
     
     
@@ -85,5 +109,67 @@ public class NetworkAdaptor {
             return base64Credentials
         }
         return ""
+    }
+    
+    public static func getPaymentUrl(
+        networkURL: MipsNetworkUrls ,
+        merchant : MerchantDetails ,
+        credentials : MerchantCredentials ,
+        amount : Amount ,
+        orderID : String
+    ) async -> URL? {
+
+
+        let json : [String : Any] = [
+            "authentify" : [
+                "id_merchant" : merchant.sIdMerchant ,
+                "id_entity" : merchant.id_entity ,
+                "id_operator" : merchant.id_operator ,
+                "operator_password" : merchant.operator_password
+            ] ,
+            "order" : [
+                "id_order" : orderID ,
+                "currency" : amount.currency.rawValue ,
+                "amount" : amount.price
+            ] ,
+            "iframe_behavior" : [
+                "custom_redirection_url" : "" ,
+                "language" : "EN"
+            ] ,
+            "request_mode" : "simple" ,
+            "touchpoint" : "native_app"
+        ]
+        let (body , bodyError) = JSONCoder.encodeJson(json: json)
+        if let bodyError = bodyError {
+            LogManager.shared.error(bodyError.localizedDescription)
+            return nil
+        }
+        let req = NSMutableURLRequest(url: URL(string: networkURL.paymentUrlGenerator)!  )
+        req.httpBody = body
+        req.httpMethod = "POST"
+        req.setValue("Basic \(getBasicAuthWithusernamePassword(username: credentials.username, password: credentials.password))", forHTTPHeaderField: "Authorization")
+        
+        let (res , err) = await HttpHandler.request(req as URLRequest)
+        if let err = err {
+            LogManager.shared.error(err.localizedDescription)
+            return nil
+        }
+        
+        let (decode , decodeErr) = JSONCoder.decodeJson(data: res!)
+        if let decodeErr = decodeErr {
+            LogManager.shared.error(decodeErr.localizedDescription)
+            return nil
+        }
+        guard let answer = decode["answer"] as? [String : Any],
+              let operation_status = answer["operation_status"] as? String ,
+              operation_status.lowercased() == "success".lowercased() ,
+              let payment_zone_data = answer["payment_zone_data"] as? String ,
+              let finalUrl = URL(string: payment_zone_data)
+        else {
+            LogManager.shared.error("unable to get payment url")
+            return nil
+        }
+            
+        return finalUrl
     }
 }
--- Podfile
+++ Podfile
@@ -1,29 +1,16 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
 
-target 'DemoApp' do
-  # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-  # pod 'CryptoSwift', '~> 1.8.2'
-
-  # # pod 'CocoaLumberjack'
-  # pod 'CocoaLumberjack/Swift'
-
-  # pod 'Alamofire'
-  # Pods for DemoApp
-
-end
-
 target 'MIPS_iOS_SDK' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'CryptoSwift', '~> 1.8.2'
+  # pod 'CryptoSwift', '~> 1.8.2'
 
   # pod 'CocoaLumberjack'
-  pod 'CocoaLumberjack/Swift'
+  # pod 'CocoaLumberjack/Swift'
 
-  pod 'Alamofire'
+  # pod 'Alamofire'
   # Pods for MIPS_iOS_SDK
 
 end
